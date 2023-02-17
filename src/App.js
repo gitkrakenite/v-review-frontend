585ef878c9d6d354114516ba00c181a62eecf949
gitkraken303 <johnspaxxvile@gmail.com>
2023-02-17 12:18:54 +0300

    react folder structure and routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Splash from "./screens/splash/Splash";
+import Landing from "./screens/landing/Landing";
+import Auth from "./screens/auth/Auth";
+import Profile from "./screens/profile/Profile";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Splash />} />
+          <Route path="/home" element={<Landing />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/me" element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
